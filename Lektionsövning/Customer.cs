0882852e0f11f1aa7b7a71dad6e824372e1169a9
Ellinor Vonck <pellinor99@hotmail.com>
2022-11-01 10:59:55 +0100

    Add project files.

---
+++ Lektionsövning/Customer.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lektionsövning
+{
+    class Customer
+    {
+        private int id = 0;
+        private string firstname = "";
+        private string lastname = "";
+        private string email = "";
+
+
+        public Customer(int id, string firstname, string lastname, string email)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+        }
+        public Customer()
+        {
+
+        }
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("ID får inte vara negativt");
+                }
+                else
+                {
+                    this.id = value;
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(firstname))
+                {
+                    return "Name has no value";
+                }
+                return firstname;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You need to provide a value");
+                }
+                else
+                {
+                    this.firstname = value;
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(lastname))
+                {
+                    return "Lastname has no value";
+                }
+                return lastname; ;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You need to provide a value");
+                }
+                else
+                {
+                    this.lastname = value;
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    return "Email has no value";
+                }
+                return email; ;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You have to provide a value");
+                }
+                else
+                {
+                    this.email = value;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return "ID: " + ID + " Firstname: " + FirstName + " Lastname: " + LastName + " Email: " + Email;
+        }
+    }
+}
+
---
+++ Lektionsövning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lektionsövning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer(10, " Pelle", "Pellson", "PelPellson@gmail.com");
+            Customer C2 = new Customer(11, " Palle", "Pallson", "PalPalsson@gmail.com");
+            Customer C3 = new Customer();
+
+            Console.WriteLine(C1);
+            Console.WriteLine(C2);
+            Console.WriteLine(C3);
+        }
+    }
+}
